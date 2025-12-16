de16bc1160956303be939feb1fb1f13c6554222b
wak4kusa296 <162152488+wak4kusa296@users.noreply.github.com>
2025-12-16 13:01:49 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'shinbun-pwa-v1';
+const ASSETS = [
+  './NewsEditer.HTML',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  if (request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request).then((resp) => {
+      const clone = resp.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(request, clone));
+      return resp;
+    }).catch(() => cached))
+  );
+});
